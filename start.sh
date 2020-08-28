425de1e77dc1598748bf28c41f71472aac6d6dd1
M3sterKw0n <48968473+M3sterKw0n@users.noreply.github.com>
2020-08-28 19:43:01 -0400

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/M3sterKw0n/cyber_extra/master/supplement.sh
-wget https://raw.githubusercontent.com/M3sterKw0n/cyber_extra/master/addusers.sh
-wget https://raw.githubusercontent.com/M3sterKw0n/cyber_extra/master/supplement2.sh
-wget https://raw.githubusercontent.com/M3sterKw0n/cyber_extra/master/supplement3.sh
-wget https://raw.githubusercontent.com/M3sterKw0n/cyber_extra/master/shadow1
-wget https://raw.githubusercontent.com/M3sterKw0n/cyber_extra/master/shadow2
-wget https://raw.githubusercontent.com/M3sterKw0n/cyber_extra/master/shadow3
-wget https://raw.githubusercontent.com/M3sterKw0n/cyber_extra/master/shadow4
+wget https://raw.githubusercontent.com/M3sterKw0n/cyber_extra/master/supplement.sh > /dev/null 2>&1
+wget https://raw.githubusercontent.com/M3sterKw0n/cyber_extra/master/addusers.sh > /dev/null 2>&1
+wget https://raw.githubusercontent.com/M3sterKw0n/cyber_extra/master/supplement2.sh > /dev/null 2>&1
+wget https://raw.githubusercontent.com/M3sterKw0n/cyber_extra/master/supplement3.sh > /dev/null 2>&1
+wget https://raw.githubusercontent.com/M3sterKw0n/cyber_extra/master/shadow1 > /dev/null 2>&1
+wget https://raw.githubusercontent.com/M3sterKw0n/cyber_extra/master/shadow2 > /dev/null 2>&1
+wget https://raw.githubusercontent.com/M3sterKw0n/cyber_extra/master/shadow3 > /dev/null 2>&1
+wget https://raw.githubusercontent.com/M3sterKw0n/cyber_extra/master/shadow4 > /dev/null 2>&1
