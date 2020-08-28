75aa46d19c7e10c6bef537ca64329e304145a175
M3sterKw0n <48968473+M3sterKw0n@users.noreply.github.com>
2020-08-28 19:04:24 -0400

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/M3sterKw0n/cyber_extra/master/supplement.sh
+wget https://raw.githubusercontent.com/M3sterKw0n/cyber_extra/master/addusers.sh
+wget https://raw.githubusercontent.com/M3sterKw0n/cyber_extra/master/supplement2.sh
+wget https://raw.githubusercontent.com/M3sterKw0n/cyber_extra/master/supplement3.sh
+wget https://raw.githubusercontent.com/M3sterKw0n/cyber_extra/master/shadow1
+wget https://raw.githubusercontent.com/M3sterKw0n/cyber_extra/master/shadow2
+wget https://raw.githubusercontent.com/M3sterKw0n/cyber_extra/master/shadow3
+wget https://raw.githubusercontent.com/M3sterKw0n/cyber_extra/master/shadow4
